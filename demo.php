ef9af5845399eb993623b94edd35c1f92ce7c23e
wanglifu <358630827@qq.com>
2019-12-26 17:11:32 +0800

    test

---
+++ demo.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *   ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　＞　　　＜　┃
+ * ┃　　　　　　　┃
+ * ┃...　⌒　...　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ *     ┃　　　┃　
+ *     ┃　　　┃
+ *     ┃　　　┃
+ *     ┃　　　┃  神兽保佑
+ *     ┃　　　┃  代码无bug　　
+ *     ┃　　　┃
+ *     ┃　　　┗━━━┓
+ *     ┃　　　　　　　┣┓
+ *     ┃　　　　　　　┏┛
+ *     ┗┓┓┏━┳┓┏┛
+ *       ┃┫┫　┃┫┫
+ *       ┗┻┛　┗┻┛
+ * Created by PhpStorm.
+ * User: wanglifu
+ * Date: 2019/12/26
+ * Time: 17:09
+ * Notes:
+ */
+require_once './vendor/autoload.php';
+
+\Abstarfish\Starfish\Tools::Hello();
