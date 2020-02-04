1121cbb92fc69c72652d8995cb3d375e5b4b1ae1
wanglifu <358630827@qq.com>
2020-02-04 12:10:50 +0800

    111

---
+++ src/Ford/Focus/Focus2016.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Apple
+ * Date: 2020/2/4
+ * Time: 11:34
+ */
+namespace Ford\Focus;
+class Focus2016
+{
+    public function info()
+    {
+        echo "This is Ford Focus2016!<br>";
+    }
+}
---
+++ src/Ford/Fusion/Fusion2016.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Apple
+ * Date: 2020/2/4
+ * Time: 11:34
+ */
+namespace Ford\Fusion;
+class Fusion2016
+{
+    public function info()
+    {
+        echo 'This is Ford Fusion2016 !<br>';
+    }
+}
